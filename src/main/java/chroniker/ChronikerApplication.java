07f9aa2e68459cef3e32248dfc3f3a51aa2c9494
rajannarvar <rajan@narvar.com>
2018-10-19 18:05:59 -0700

    SERV-823 setting up repo and base project

---
+++ src/main/java/chroniker/ChronikerApplication.java
@@ -0,0 +1,13 @@
+package chroniker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class ChronikerApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(ChronikerApplication.class, args);
+    }
+}
