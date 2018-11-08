74bdafdbe9b6dc16a05d25a997edffea0e3402f4
rajannarvar <rajan@narvar.com>
2018-11-08 11:47:07 -0800

    SERV-823 removing unsused imports

--- src/main/java/com/narvar/chroniker/controller/ChronikerController.java
+++ src/main/java/com/narvar/chroniker/controller/ChronikerController.java
@@ -1,14 +1,8 @@
 package com.narvar.chroniker.controller;
 
 
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 @RestController
 @RequestMapping("/chroniker")
